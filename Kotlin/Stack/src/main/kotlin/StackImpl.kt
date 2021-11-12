e26768f7addcc050fc576e2164c905e300254bab
rawaha-coder <rawaha.coder@gmail.com>
2021-11-12 14:26:37 +0100

    rename stack data structure folder

--- Kotlin/Stack/src/main/kotlin/StackImpl.kt
+++ Kotlin/Stack/src/main/kotlin/StackImpl.kt
@@ -1,16 +1,5 @@
-package doublestack
-
 class StackImpl<T : Any> : Stack<T> {
     private val storage = arrayListOf<T>()
-
-    override fun toString() = buildString {
-        appendLine("----top----")
-        storage.asReversed().forEach {
-            appendLine("$it")
-        }
-        appendLine("-----------")
-    }
-
     override fun push(element: T) {
         storage.add(element)
     }
@@ -29,7 +18,10 @@ class StackImpl<T : Any> : Stack<T> {
     override val count: Int
         get() = storage.size
 
-    companion object {
+    val isEmpty: Boolean
+        get() = count == 0
+
+    companion object{
         fun <T : Any> create(items: Iterable<T>): Stack<T> {
             val stack = StackImpl<T>()
             for (item in items) {
@@ -38,8 +30,12 @@ class StackImpl<T : Any> : Stack<T> {
             return stack
         }
     }
-}
 
-fun <T : Any> stackOf(vararg elements: T): Stack<T> {
-    return StackImpl.create(elements.asList())
+    override fun toString() = buildString {
+        appendLine("----top----")
+        storage.asReversed().forEach {
+            appendLine("$it")
+        }
+        appendLine("-----------")
+    }
 }
