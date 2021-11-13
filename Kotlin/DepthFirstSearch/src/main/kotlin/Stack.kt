f974d9400ed524ef33f7ddb58343791bab1928c4
rawaha-coder <rawaha.coder@gmail.com>
2021-11-13 15:02:12 +0100

    Kotlin Depth First Search Algorithms

---
+++ Kotlin/DepthFirstSearch/src/main/kotlin/Stack.kt
@@ -0,0 +1,69 @@
+interface Stack<T: Any> {
+
+    /**
+     * Push of an T into the stack.Stack
+     */
+    fun push(element: T)
+
+    /**
+     * Pops an element from the stack.Stack if any or returns null.
+     */
+    fun pop(): T?
+
+    val count: Int
+        get
+
+    fun peek(): T?
+
+    val isEmpty: Boolean
+        get() = count == 0
+}
+
+
+/**
+ * Simple stack.Stack implementation using an ArrayList
+ */
+class StackImpl<T: Any> : Stack<T> {
+
+    private val storage = arrayListOf<T>()
+
+    companion object {
+        fun <T: Any> create(items: Iterable<T>): Stack<T> {
+            val stack = StackImpl<T>()
+            for (item in items) {
+                stack.push(item)
+            }
+            return stack
+        }
+    }
+
+    override fun push(element: T) {
+        storage.add(element)
+    }
+
+    override fun pop(): T? {
+        if (isEmpty) {
+            return null
+        }
+        return storage.removeAt(count - 1)
+    }
+
+    override fun peek(): T? {
+        return storage.lastOrNull()
+    }
+
+    override val count: Int
+        get() = storage.size
+
+    override fun toString() = buildString {
+        appendLine("----top----")
+        storage.asReversed().forEach {
+            appendLine("$it")
+        }
+        appendLine("-----------")
+    }
+}
+
+fun <T: Any> stackOf(vararg elements: T): Stack<T> {
+    return StackImpl.create(elements.asList())
+}
