fe02b93567bd6d123ddf8f074a3045f019e67584
rawaha-coder <rawaha.coder@gmail.com>
2021-11-13 10:59:14 +0100

    Kotlin AVL tree

---
+++ Kotlin/AVLtree/src/main/kotlin/Main.kt
@@ -0,0 +1,25 @@
+fun main() {
+
+    println("\n*** AVL tree code ***\n")
+
+    "repeated insertions in sequence" example {
+        val tree = AVLTree<Int>()
+
+        (0..14).forEach {
+            tree.insert(it)
+        }
+
+        print(tree)
+    }
+
+    "removing a value" example {
+        val tree = AVLTree<Int>()
+        tree.insert(15)
+        tree.insert(10)
+        tree.insert(16)
+        tree.insert(18)
+        print(tree)
+        tree.remove(10)
+        print(tree)
+    }
+}
